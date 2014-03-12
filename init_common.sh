4e9987ac3786dd6116b434a45db7e4e27c18059b
liss <coder.liss@gmail.com>
2014-03-11 23:34:28 -0700

    fix bugs

--- init_common.sh
+++ init_common.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt-get install build-essential
-sudo apt-get install ctags cscope
+sudo apt-get -y install build-essential
+sudo apt-get -y install ctags cscope
 
 # install vim
-sudo apt-get install vim
-sudo apt-get install kernel-package libncurses5-dev fakeroot wget bzip2
+sudo apt-get -y install vim bc
+sudo apt-get -y install kernel-package libncurses5-dev fakeroot wget bzip2
 git clone https://github.com/coderliss/vim-config.git
 cd vim-config; ./install.sh ; cd -
--- init_kernel.sh
+++ init_kernel.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #mainline
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git $HOME
-cd $HOME; make defconfig; make tags; cd -
-sed -i '/$/aset tags+=$HOME/linux/tags' $HOME/.vimrc
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git $HOME/linux
+cd $HOME/linux; make defconfig; make tags; cd -
+sed -i '$aset tags+=$HOME/linux/tags' $HOME/.vimrc
 
 #stable 
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git $HOME
-cd $HOME; make defconfig; make tags; cd -
-sed -i '/$/aset tags+=$HOME/linux-stable/tags' $HOME/.vimrc
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git $HOME/linux-stable
+cd $HOME/linux-stable; make defconfig; make tags; cd -
+sed -i '$aset tags+=$HOME/linux-stable/tags' $HOME/.vimrc
 
 #other resource
-git clone git://git.sv.gnu.org/coreutils $HOME
-git clone https://git.fedorahosted.org/git/linux-pam.git $HOME
-git clone git://sourceware.org/git/glibc.git $HOME
-git clone git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git $HOME
+git clone git://git.sv.gnu.org/coreutils $HOME/coreutils
+git clone https://git.fedorahosted.org/git/linux-pam.git $HOME/linux-pam
+git clone git://sourceware.org/git/glibc.git $HOME/glibc
+git clone git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git $HOME/util-linux
