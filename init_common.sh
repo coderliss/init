e60872351757c5365924fd39f3fe5f5178f53349
Jimmy Li <coder.liss@gmail.com>
2014-05-23 12:03:50 +0800

    add clone mutt-config.git

--- init_common.sh
+++ init_common.sh
@@ -16,6 +16,7 @@ sudo apt-get -y install vim bc
 sudo apt-get -y install kernel-package libncurses5-dev fakeroot wget bzip2
 git clone https://github.com/coderliss/vim-config.git
 cd vim-config; ./install.sh ; cd -
+git clone https://github.com/coderliss/mutt-config.git
 
 # setup git
 git config --global user.name "Jimmy Li"
